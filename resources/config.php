cc5ec8482a07bcbf62259dff168b43f65519d460
DS09AT <76005808+DS09AT@users.noreply.github.com>
2022-07-10 20:29:41 +0200

    wip

--- resources/config.php
+++ resources/config.php
@@ -4,20 +4,20 @@ return [
     'oauthconf' => [ // See http://oauth2-client.thephpleague.com/usage/#authorization-code-flow
         'clientId' => 'demoapp', // The client ID assigned to you by the provider
         'clientSecret' => 'demopass', // The client password assigned to you by the provider
-        'redirectUri' => 'http://example.com/your-redirect-url/',
-        'urlAuthorize' => 'http://brentertainment.com/oauth2/lockdin/authorize',
-        'urlAccessToken' => 'http://brentertainment.com/oauth2/lockdin/token',
-        'urlResourceOwnerDetails' => 'http://brentertainment.com/oauth2/lockdin/resource',
+        'redirectUri' => 'http://example.com/oauth/callback',
+        'urlAuthorize' => 'http://example.com/oauth/authorize',
+        'urlAccessToken' => 'http://example.com/oauth/token',
+        'urlResourceOwnerDetails' => 'http://example.com/oauth/resource',
     ],
     'provider' => \Kronthto\LaravelOAuth2Login\OAuthProvider::class,
 
-    'oauth_redirect_path' => '/oauth2/callback',
+    'oauth_redirect_path' => '/oauth/callback',
 
-    'session_key' => 'oauth2_session',
-    'session_key_state' => 'oauth2_auth_state',
+    'session_key' => 'oauth_session',
+    'session_key_state' => 'oauth_auth_state',
 
-    'resource_owner_attribute' => 'oauth2_user',
-    'auth_driver_key' => 'oauth2',
+    'resource_owner_attribute' => 'oauth_user',
+    'auth_driver_key' => 'oauth',
     'authWrapperFactory' => null, // Can be used to specify a factory with an __invoke (ResourceOwnerInterface passed as arg1) method to build a custom User object
     'authWrapper' => \Kronthto\LaravelOAuth2Login\OAuthUserWrapper::class, // Ignored if authWrapperFactory is not null
 
